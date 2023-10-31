a907440d0f6bbb647881a6110b8e6600498d7cf9
vriskaserket51 <serketvriska51@gmail.com>
2023-10-31 16:23:36 +0900

    Indented

--- gii_bin.py
+++ gii_bin.py
@@ -183,16 +183,16 @@ if __name__ == '__main__':
 		os.environ[ 'GII_ENV_SET' ] = 'DONE'
 		
 		class Unbuffered(object):
-			 def __init__(self, stream):
-					 self.stream = stream
-			 def write(self, data):
-					 self.stream.write(data)
-					 self.stream.flush()
-			 def writelines(self, datas):
-					 self.stream.writelines(datas)
-					 self.stream.flush()
-			 def __getattr__(self, attr):
-					 return getattr(self.stream, attr)
+			def __init__(self, stream):
+					self.stream = stream
+			def write(self, data):
+					self.stream.write(data)
+					self.stream.flush()
+			def writelines(self, datas):
+					self.stream.writelines(datas)
+					self.stream.flush()
+			def __getattr__(self, attr):
+					return getattr(self.stream, attr)
 
 		import sys
 		sys.stdout = Unbuffered(sys.stdout)
